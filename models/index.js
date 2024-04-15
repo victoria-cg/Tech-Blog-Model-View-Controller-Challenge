d444d673a2af7cfc958325c337288b62e25ec1ff
Victoria G <125226074+victoria-cg@users.noreply.github.com>
2024-04-14 21:11:30 -0500

    use sequelize in both seed functions and create relationships between tables

--- models/index.js
+++ models/index.js
@@ -1,6 +1,22 @@
 const User = require('./User');
 const Blog = require('./Blog')
 
-// User.hasMany
+//establish database relationships: see bootcamp module 13 on ORM for more examples/explanation
+//blog has one user, if user is deleted this cascades to their blogs and deletes them too, user id is foreign key referencing id column in user table
+Blog.hasOne(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+  });
+  
+  Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+  
+  // Define a user as having many blogs, creates a foreign key in the `blog` table
+  User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+  });
+  
 
 module.exports = { User, Blog };
