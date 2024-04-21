b28091ae2e68360b2d0a53e69581341f0f608029
Victoria G <125226074+victoria-cg@users.noreply.github.com>
2024-04-21 15:17:59 -0500

    moved get route to home routes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -15,28 +15,28 @@ router.delete("/:id", withAuth, async(req,res) => {
     }})
 })
 
-//GET API route for all the blogs associated with the logged in user
+//GET API route for all the blogs associated with the logged in user: MOVED TO homeRoutes.js, incorrectly placed in API routes but it's front end facing
 //user's own blogs are returned at the api/blogs/dashboard endpoint
-router.get("/dashboard", withAuth, async(req,res) => {
-   try {
-    //findAll return an array of objects from the blog model/data where the user is the specific logged in user
-    //try catch block allows us to catch and log error responses
-    const userBlogData = await Blog.findAll({where: {
-        user_id: req.session.user_id,
-    }}).catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-//.map takes one array, does another function on that array data, an returns a new array of results
-//in this case we take the array of user-specific blog objects from userBlogData, and we use {plain:true} to serialize the data so we only get the infomration we need and put it into a new array called 'blogs'
-    const userBlogs = userBlogData.map((userBlog) => userBlog.get({plain: true}));
-    //response renders handlebars.js dashboard view and passes it the serialized user-specific blog data, session/user login/auth information
-    res.render('dashboard', {userBlogs, logged_in: req.session.logged_in});
-   } catch (error) {
-    //catch and respond with error message if not successful
-    res.status(500).json(err);
-   }
-});
+// router.get("/dashboard", withAuth, async(req,res) => {
+//    try {
+//     //findAll return an array of objects from the blog model/data where the user is the specific logged in user
+//     //try catch block allows us to catch and log error responses
+//     const userBlogData = await Blog.findAll({where: {
+//         user_id: req.session.user_id,
+//     }}).catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// //.map takes one array, does another function on that array data, an returns a new array of results
+// //in this case we take the array of user-specific blog objects from userBlogData, and we use {plain:true} to serialize the data so we only get the infomration we need and put it into a new array called 'blogs'
+//     const userBlogs = userBlogData.map((userBlog) => userBlog.get({plain: true}));
+//     //response renders handlebars.js dashboard view and passes it the serialized user-specific blog data, session/user login/auth information
+//     res.render('dashboard', {userBlogs, logged_in: req.session.logged_in});
+//    } catch (error) {
+//     //catch and respond with error message if not successful
+//     res.status(500).json(err);
+//    }
+// });
 
 
 module.exports = router
